67f3d8bfec1663515050594314b81637cd077996
yarikama <amaki30226@gmail.com>
2023-12-19 19:24:46 +0800

    header.h

---
+++ header.h
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <vector>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
