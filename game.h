6619d1eb9774f790d748275d2545d9ca3ad57652
yarikama <amaki30226@gmail.com>
2023-12-19 20:45:46 +0800

    Class game error fixing

--- game.h
+++ game.h
@@ -29,15 +29,15 @@ public:
     void broadcast_xy(Packet packet, int sockfd);
     void handle_client(int sockfd);
 private:
-    unordered_map<const char*, struct Player> players;
+    unorder_map<const char*, struct Player> players;
 };
 
-Game::add_player(char* name, struct Player new_player){ players[name] = new_player; }
-Game::remove_player(char* name){ players.erase(name); }
-Game::get_player_size(){ return players.size(); }
-Game::serialize(Packet packet, char* buffer){ memcpy(buffer, &packet, sizeof(Packet)); }
-Game::deserialize(Packet packet, char* buffer){ memcpy(&packet, buffer, sizeof(Packet)); }
-Game::broadcast_xy(Packet packet, int sockfd){
+void Game::add_player(char* name, struct Player new_player){ players[name] = new_player; }
+void Game::remove_player(char* name){ players.erase(name); }
+void Game::get_player_size(){ return players.size(); }
+void Game::serialize(Packet packet, char* buffer){ memcpy(buffer, &packet, sizeof(Packet)); }
+void Game::deserialize(Packet packet, char* buffer){ memcpy(&packet, buffer, sizeof(Packet)); }
+void Game::broadcast_xy(Packet packet, int sockfd){
     char buffer[MAXLINE];
     for (auto& player : players){
         if(player.second.sockfd != sockfd){
