687d810d08cba842d7c7f74d1fa6d35a6133a0a4
yarikama <amaki30226@gmail.com>
2023-12-19 19:34:14 +0800

    header更新 port code

--- header.h
+++ header.h
@@ -1,3 +1,5 @@
+#ifndef header_h
+#define header_h
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
@@ -7,3 +9,9 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#define mapMode 0
+#define chatMode 1
+#define requestMode 2
+#define gamePort 11130
+#define serverIP "127.0.0.1"
+#endif
