f744644e841c07f1da9a929f51075cd4732972d9
yuhansun33 <yuhan.c@nycu.edu.tw>
2023-12-19 19:25:28 +0800

    update

--- server.cpp
+++ server.cpp
@@ -1,4 +1,5 @@
 #include "header.h"
+#include "game.h"
 using namespace std;
 
 void sig_chld(int signo){
@@ -9,52 +10,9 @@ void sig_chld(int signo){
     return;
 }
 
-struct Packet{
-	int mode_packet; //MAP , REQUEST, CHAT
-	char sender_name[MAXLINE]; //sender name
-    char receiver_name[MAXLINE]; //receiver name
-	float x_packet;
-	float y_packet;
-	char message[MAXLINE];
-};
-struct Player{
-	int sockfd;
-    int mode_player; //MAP , CHAT
-	float x_player;
-	float y_player;
-};
 
-class Game{
-public:
-    Game();
-    void add_player(struct player new_player);
-    void remove_player(int sockfd);
-    void get_player_size();
-    void serialize(Packet packet, char* buffer);
-    void deserialize(Packet packet, char* buffer);
-    void broadcast_xy(Packet packet, int sockfd);
-    void handle_client(int sockfd);
 
-private:
-    unordered_map<const char*, struct Player> players;
 
-};
-
-Game::add_player(char* name, struct Player new_player){ players[name] = new_player; }
-Game::remove_player(char* name){ players.erase(name); }
-Game::get_player_size(){ return players.size(); }
-Game::serialize(Packet packet, char* buffer){ memcpy(buffer, &packet, sizeof(Packet)); }
-Game::deserialize(Packet packet, char* buffer){ memcpy(&packet, buffer, sizeof(Packet)); }
-Game::broadcast_xy(Packet packet, int sockfd){
-    char buffer[MAXLINE];
-    for (auto& player : players){
-        if(player.second.sockfd != sockfd){
-            //send
-            serialize(packet, buffer);
-            write(player.second.sockfd, buffer, sizeof(Packet));
-        }
-    }
-}
 // Game::handle_client(int sockfd, char* name){
 //     int n;
 //     char buffer[MAXLINE], sendline[MAXLINE];
