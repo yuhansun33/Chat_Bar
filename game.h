5f70e5cd982567af5691ded720c2a43e6492e80a
yuhansun33 <yuhan.c@nycu.edu.tw>
2023-12-20 13:21:29 +0800

    fix get_player_size() datatype

--- game.h
+++ game.h
@@ -7,7 +7,7 @@ public:
     Game();
     void add_player(char* name, struct Player new_player);
     void remove_player(char* name);
-    void get_player_size();
+    int  get_player_size();
     void serialize(Packet packet, char* buffer);
     void deserialize(Packet packet, char* buffer);
     void broadcast_xy(Packet packet, int sockfd);
