e475633c77fdf2b3c8993c852cb0590681042b47
yuhansun33 <yuhan.c@nycu.edu.tw>
2023-12-21 15:20:49 +0800

    update class structure

--- server.cpp
+++ server.cpp
@@ -1,108 +1,12 @@
 #include "header.h"
-#include "game.h"
+#include "serverTCP.h"
 using namespace std;
 
-void sig_chld(int signo){
-    pid_t   pid;
-    int     stat;
-
-    while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0);
-    return;
-}
-
 int main() {
-    int                 n;
-	int			        listenfd, connfd, sockfd;
-	socklen_t		    clilen;
-	struct sockaddr_in	cliaddr, servaddr;
-    char                recvline[MAXLINE], sendline[MAXLINE];
-    time_t			    ticks;
-    FILE			    *fp;
+    serverTCP serverTCP;
 
-    int maxfd;
-    fd_set rset, allset;
-
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family      = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(GAMEPORT);
-
-    bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
-    listen(listenfd, LISTENQ);
-    signal(SIGCHLD, sig_chld);
-
-    maxfd = listenfd;
-    FD_ZERO(&rset);
-    FD_ZERO(&allset);
-    FD_SET(listenfd, &allset);
-
-    Game game;
-
-    while(true){
-        rset = allset;
-        n = select(maxfd + 1, &rset, NULL, NULL, NULL);
-        //listenfd
-        if(FD_ISSET(listenfd, &rset)){
-            clilen = sizeof(cliaddr);
-            //accept
-            if ( (connfd = accept(listenfd, (SA *) &cliaddr, &clilen)) < 0) {
-                if (errno == EINTR){
-                    continue;
-                }else{
-                    perror("accept error");
-                }
-            }
-            //init packet
-            Packet init_packet(INITMODE, "", "", (float)game.get_player_size(), 0, "");
-            game.sendData(init_packet, connfd);
-            //送所有人位置
-            for (auto& player : game.get_players_map()){
-                Packet packet(MAPMODE, player.first, "", player.second.x_player, player.second.y_player, "");
-                game.sendData(packet, connfd);
-            }
-            //讀 ID
-            Packet packet = game.receiveData(connfd);
-            //放入 vector
-            Player new_player(connfd, MAPMODE, packet.x_packet, packet.y_packet);
-            game.add_player(packet.sender_name, new_player);
-            
-            FD_SET(connfd, &allset);
-            if(connfd > maxfd) maxfd = connfd;
-        }
-        cout << "player num : " << (float)game.get_player_size() << endl;
-        //看每個 client
-        for (const auto& player : game.get_players_map() ){
-            sockfd = player.second.sockfd;
-            // cout << "2)sockfd: " << sockfd << "player name: " << player.first << endl;
-            if(FD_ISSET(sockfd, &rset)){
-                //handle_client
-                Packet packet = game.receiveData(sockfd);
-                cout << "sender: " << packet.sender_name << " (" << packet.x_packet << ", " << packet.y_packet << ")" << endl;
-                if(packet.mode_packet == MAPMODE){
-                    //map mode
-                    Packet new_packet(MAPMODE, packet.sender_name, "", packet.x_packet, packet.y_packet, "");
-                    game.broadcast_xy(new_packet, sockfd);
-                }else if(packet.mode_packet == REQMODE){
-                    //request mode
-                    int receiver_sockfd = game.get_player_sockfd(packet.receiver_name);
-                    if(packet.message == "1st request\n"){
-                        Packet new_packet(REQMODE, packet.receiver_name, packet.sender_name, 0, 0, "Connect?\n");
-                        game.sendData(new_packet, receiver_sockfd);
-                    }else if(packet.message == "Yes\n"){
-                        Packet new_packet(REQMODE, packet.receiver_name, packet.sender_name, 0, 0, "Can connect.\n");
-                        game.sendData(new_packet, receiver_sockfd);
-                    }else if(packet.message == "No\n"){
-                        Packet new_packet(REQMODE, packet.receiver_name, packet.sender_name, 0, 0, "Can't connect.\n");
-                        game.sendData(new_packet, receiver_sockfd);
-                    }
-                }
-            }
-        }
-
-
-    }
+    serverTCP.TCP_connect(GAMEPORT);
+    serverTCP.game_mainloop();
 
     return 0;
 }
--- serverTCP.h
+++ serverTCP.h
@@ -3,16 +3,42 @@
 #include "header.h"
 #include "elementTCP.h"
 class serverTCP{
+//share
 public:
     serverTCP(){};
-    void add_player(char* name, struct Player new_player);
-    void remove_player(char* name);
-    int  get_player_size();
+    static void sig_chld(int signo);
+    void TCP_connect(int port);
+    void accept_client();
+//login server
+    void login_mainloop();
+    void login_handle();
+//game server
+    void game_mainloop();
+    void new_game_handle();
+    void game_handle();
     std::string serialize(Packet packet);
     Packet deserialize(std::string& json_string);
     void sendData(Packet packet, int sockfd);
     Packet receiveData(int sockfd);
     void broadcast_xy(Packet packet, int sockfd);
+    
+private:
+	int			        n, listenfd, connfd, sockfd;
+	socklen_t		    clilen;
+	struct sockaddr_in	cliaddr, servaddr;
+    int                 maxfd;
+    fd_set              rset, allset;
+
+    std::unordered_map<std::string, struct Player> players;
+};
+
+class PlayerList{
+    friend class serverTCP;
+public:
+    PlayerList(){};
+    void add_player(char* name, struct Player new_player);
+    void remove_player(char* name);
+    int  get_player_size();
     std::unordered_map<std::string, struct Player> get_players_map();
     void show_players();
     int get_player_sockfd(char* name);
