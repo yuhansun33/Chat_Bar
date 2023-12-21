b9b534c2aa7184f00eeb9f6a81087963f354dbcc
yuhansun33 <yuhan.c@nycu.edu.tw>
2023-12-21 17:05:01 +0800

    put serverSQL in serverTCP

--- serverTCP.h
+++ serverTCP.h
@@ -46,4 +46,25 @@ private:
     std::unordered_map<std::string, struct Player> players;
 };
 
+class sqlServer{
+    friend class serverTCP;
+public:
+    sqlServer(){};
+    sqlServer(std::string user_name, std::string user_password);
+    void db_connect();
+    void db_query();
+    void db_clear();
+    void db_pswd_select();
+    bool login_check();
+    void db_user_insert();
+    bool db_register();
+private:
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+    sql::PreparedStatement *prep_stmt;
+    sql::ResultSet *res;
+    int affectedRows;
+    std::string user_name;
+    std::string user_password; 
+};
 #endif
