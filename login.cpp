83f11b48724a9f108452b2b3d7b48be6e413dd1e
yarikama <amaki30226@gmail.com>
2023-12-23 00:21:24 +0800

    error fix name

--- login.cpp
+++ login.cpp
@@ -20,7 +20,7 @@ bool Login(ClientConnectToServer& client, std::string& username, std::string& pa
     Packet packet2 = client.receiveData();
     if (packet2.mode_packet == LOGINMODE && strcmp(packet2.message, "login success") == 0) {
         std::cout << "Login success!" << std::endl;
-        execlp("./main", "./main", username.c_str(), (char*)NULL);
+        execlp("./main", "./main", packet2.sender_name, (char*)NULL);
         return true;
     } else {
         std::cout << "Login failed!" << std::endl;
